5849252d8610020cbc0d71439c119e3b50ac052e
spring-hh <spring.hh.shu@gmail.com>
2021-01-21 00:21:12 +0900

    add git_version

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-# Scrapingの勉強始めるぞい!!
+from bs4 import BeautifulSoup
+import requests
+
+html = requests.get('https://git-scm.com')
+soup = BeautifulSoup(html.text, 'lxml')
+
+git_version = soup.findAll('span', {'class': 'version'})
+print(git_version[0].text)
